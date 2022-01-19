2e4922474c66a97ed1f63627add7ca3687e563ad
DESKTOP-3J65I2E\admin <RogerDTilton@gmail.com>
2022-01-20 05:25:33 +0900

    add event

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,9 +1,10 @@
-import {Hello} from "../types";
+import {Hello, Where} from "../types";
 import { MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam';
 import { BigNumber } from "ethers";
 
 // Setup types from ABI
 type HelloEventArgs = [string, BigNumber] & { sender: string; hi: BigNumber; };
+type WhereEventArgs = [string, string] & {here: string; there: string; }
 
 export async function handleHelloEvent(event: MoonbeamEvent<HelloEventArgs>): Promise<void> {
     const hello = new Hello(event.transactionHash);
@@ -12,4 +13,13 @@ export async function handleHelloEvent(event: MoonbeamEvent<HelloEventArgs>): Pr
     hello.sender = event.args.sender;
 
     await hello.save();
+}
+
+export async function handleWhereEvent(event: MoonbeamEvent<WhereEventArgs>): Promise<void> {
+    const where = new Where(event.transactionHash);
+
+    where.here = event.args.here;
+    where.there = event.args.there;
+
+    await where.save();
 }
